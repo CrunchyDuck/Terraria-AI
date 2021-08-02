eb936dc730185bba6ddf15289cf792ff9e4bf4b7
CrunchyDuck <43281440+CrunchyDuck@users.noreply.github.com>
2021-08-02 10:18:40 +0100

    Add relative check
    Adds a check that compares different bands of the graph to each other.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from scipy.fft import rfft, rfftfreq
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
+import shutil
 
 
 class FishingListener:
@@ -66,37 +67,50 @@ class FishingListener:
             new_frame.append(int_value)
         return new_frame
 
-    def parse_intervals_overlap(self, file, interval=0.25, times=5):
+    def parse_intervals_overlap(self, file, interval=0.25):
+        try:
+            shutil.rmtree(self.folder_output_path)  # Clear log folder.
+        except FileNotFoundError:
+            pass
+
         wf = wave.open(file, "rb")
         rate = wf.getframerate()
         num_samples = int(rate * (interval / 2))  # Read the audio in chunks of this size.
         previous_audio = wf.readframes(num_samples)
 
-        for i in range(times):
+        i = 0
+        while True:
             from_time = (interval / 2) * i
             to_time = (interval / 2) * (i + 2)
 
             curr_audio = wf.readframes(num_samples)
+            if not curr_audio:  # End of file
+                break
             audio = previous_audio + curr_audio
             audio = FishingListener.convert_bytes(audio)
 
             yf = rfft(audio)  # Gets the transform. Imaginary numbers.
             xf = rfftfreq(len(audio), 1 / wf.getframerate())  # This calculates the "grouping" of frequencies. Might be able to mess with this for my ranges
 
-            peak1 = get_range_sum(xf, yf, 215, 240)
+            peak1 = get_range_sum(xf, yf, 230, 240)
             peak2 = get_range_sum(xf, yf, 260, 330)
-            if self.threshold_check(peak1, peak2):
-                # If we find the fishing sound, display the graph of this sound so I can validate it.
-                fig, ax = plt.subplots()
-                ax.set_ylim([0, 300000])
-                ax.set_xlim([150, 600])
-                ax.plot(xf, np.abs(yf))
-                ax.set_title(f"{from_time} to {to_time}")
-                # Save the audio of the fishing data, too.
+            dip1 = get_range_sum(xf, yf, 245, 255)
+            # I want to keep the number of checks minimal so code doesn't become spaghetti.
+            # If it doesn't work, find better checks, don't add more.
+            if self.threshold_check(peak1, peak2) and self.relative_check(peak1, dip1):
+                # If we find the fishing sound, create a graph of this sound so I can validate it.
+                plt.axis([150, 600, 0, 400000])
+                plt.title(f"{from_time} to {to_time}")
+                plt.xlabel("Frequency (Hz)")
+                plt.ylabel("Amplitude")
+                plt.plot(xf, np.abs(yf))
+                # Save the audio and analysis of the fishing data.
                 self.save_audio(f"{from_time}-{to_time}", audio)
+                plt.savefig(f'./{self.folder_output_path}/{from_time}-{to_time}.png')
+                plt.close()
 
             previous_audio = curr_audio
-        plt.show()
+            i += 1
 
     def threshold_check(self, peak1, peak2):
         """This is a simple "volume" check, used to filter out low levels such as noise. Prebaked thresholds.
@@ -106,11 +120,18 @@ class FishingListener:
             Each peak is the sum of the predefined range.
         Returns: True if all thresholds are correct.
         """
-        if not peak1 > 200000:
+        if not peak1 > 250000:
             return False
-        if not peak2 > 500000:
+        if not peak2 > 700000:
             return False
 
+        return True
+
+    def relative_check(self, peak1, dip1):
+        """Runs through the checks that describe shapes relative to each other."""
+        compar_size = dip1 / peak1
+        if not compar_size < 0.75:  # Our "dip" is at most 75% the first peak.
+            return False
         return True
 
     def save_audio(self, name, PCM_data):
@@ -171,10 +192,10 @@ def get_range_sum(xf, yf, low, high):
 #exit()
 
 
-# peak 1: 215-240
+# peak 1: 220-240
 # peak 2: 260-330
 
-# dip 1: 250
+# dip 1: 240-260
 
 # TODO: I need to find a way to compare the values of my graph to its surroundings, to detect when a spike has happened.
 #  I could set specific number thresholds. This would be easiest, but will also break fastest.
